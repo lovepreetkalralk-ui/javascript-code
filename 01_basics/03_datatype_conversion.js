09cf92fea96e98cf80c0cb3c1414cf08acaa1622
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-09-28 09:28:27 +0000

    Datatype conversion

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,17 @@
+let score="33"
+console.log(typeof(score))
+let score1=Number(score)
+console.log(typeof(score1))
+
+let a="33avb"
+console.log(typeof(a))
+let a1=Number(a)
+console.log(typeof(a1))
+console.log(a1) // NaN
+
+// if we typecast null then ans is: 0
+//  if we typecast undefined then ans is:NaN
+//  if we typecast score="true" to number then ans is:1
+ let islogged=1
+ console.log(typeof islogged)
+ 
