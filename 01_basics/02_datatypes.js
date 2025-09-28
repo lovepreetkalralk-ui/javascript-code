b6d6f85283fa4e6cc24e74f2b2f9185c9358c7dc
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-09-28 08:59:57 +0000

    Data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) we are using node js not browser
+console.log(3+3)
+console.log("Lovepreet")
+
+
+let name="Lovepreet"
+let age=18
+let islogged=false
+
+/* number-> 2 to power 53
+big int
+string =>""
+null =>standalone value
+undefined =>value is not assigned yet
+symbol=> unique*/
+console.log(typeof age) //number
+console.log(typeof null) // object
+console.log(typeof undefined)// undefined
