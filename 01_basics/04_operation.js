4a1210828708a416a355b9757f248d7f528cbe18
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-09-29 16:42:17 +0000

    Operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,26 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(2**3);//power
+// console.log(3/4);
+// console.log(2%3);
+str1="hello";
+str2=" Lovepreet"
+str3=str1+str2;
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2"); // the thing is to see how strings behave 
+console.log(1+1+"2");
+console.log("1"+"1"+2);
+
+console.log(3+4*8/7) //also don't write like that give paranthesis
+console.log(true)// true print krega
+console.log(+true)//1 print krega pr aisha krna not a good practice
+console.log(+"")//ans=0 don't do this
+// while writing code maintain code readability
+let counter=100;
+counter++;
+console.log(counter);
