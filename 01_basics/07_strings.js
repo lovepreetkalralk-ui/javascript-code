e0916cc61201bf719fc9ce0d77426fbc91489b39
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-09-30 19:57:54 +0000

    strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,7 @@
+console.log("hello"+"world");
+const name="Lovepreet";
+const lastname="Kalra";
+const repocount=444;
+console.log(name+ lastname);
+// modern way to write this
+console.log(`My Name is ${name.toLower}and my repo count is ${repcount}`);
