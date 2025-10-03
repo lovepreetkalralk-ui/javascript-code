9b0a149de6a481a99c015b50f2f8c676e23904df
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-10-03 17:57:28 +0000

    strings

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -5,3 +5,21 @@ const repocount=444;
 console.log(name+ lastname);
 // modern way to write this
 console.log(`My Name is ${name.toLower}and my repo count is ${repcount}`);
+const gameName= new String('hitesh-hc');
+console.log(gameName[0]);
+console.log(gameName.__proto__);// object
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+// creating substring
+const subpart=gameName.substring(0,4);
+console.log(subpart);
+const anotherString=gameName.slice(-8,4);
+console.log(anotherString);
+const string1="     hitesh      ";
+console.log(string1);
+console.log(string1.trim());
+const url="http://www.hitesh20%@gmail.com";
+console.log(url.replace('20%','-'));
+console.log(url.includes('sundari'));// true or false
+console.log(gameName.split('-'));
