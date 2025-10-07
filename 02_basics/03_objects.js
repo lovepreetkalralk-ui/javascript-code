cb8e76562999f5e2e15ca0ed410fbb10ae925b8f
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-10-07 20:41:27 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+// using constructor-Singelton
+// using literals-not singelton
+// object literals:
+const JsUser={
+    name:"Lovepreet",
+    age:18,
+    islogges:false,
+    lastloggedIndays:["monday","tuesday"],
+    email:"lovepreet@gmail.com",
+    location:"jaipur"
+
+}
+    console.log(Js)
